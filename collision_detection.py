f66c2ae394db09622b28f2171260184714585798
samuelhavelka <125111579+samuelhavelka@users.noreply.github.com>
2024-01-06 23:55:14 -0600

    Bug fix
    Fix file path for new DQN model

--- collision_detection.py
+++ collision_detection.py
@@ -13,7 +13,8 @@ def make_env():
     # create environment with a set of obstacles
     env = Env(world_size=[40,10], 
           obstacles=[[5,1],[10,4],[15,7],[20,5],[30,3],[35,7],[25,7]])
-    robot = Robot(env=env,initial_loc=[1,5], inital_orient=0)
+    robot = Robot(env=env,initial_loc=[1,5], inital_orient=0,
+                  robot_dims=[0.8,0.8])
     return env
 
 
@@ -21,14 +22,13 @@ if __name__ == "__main__":
     env = make_env()
 
     # load trained model
-    model = DQN.load(f"logs\dqn_model_2020000_steps.zip")
+    model = DQN.load(f"models\dqn_collision_model.zip")
     model.set_env(env)
 
     vec_env = model.get_env()
     obs = vec_env.reset()
 
-    # training is done, wait for user input to run simulation with trained RL model
-    input("Training Complete...")
+    # run simulation with trained RL model
     while True:
         action, _states = model.predict(obs)
         obs, rewards, dones, info = vec_env.step(action)
